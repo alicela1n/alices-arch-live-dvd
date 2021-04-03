0e40a1bbb36c7e065bd712b426505046a68ecc07
Alice <devfusediboot@protonmail.com>
2021-04-03 13:58:37 -0700

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 printf "Building Arch Linux live iso\n"
+mkdir workingdir iso > /dev/null
 sudo mkarchiso -v -w workingdir -o iso archlive
+printf "Make sure nothing is mounted inside the workingdir before removing it!\n"
